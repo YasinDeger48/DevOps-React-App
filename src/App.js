ccc8b6279664f63e2c851192c448649a0dd3c85e
Yasin Değer <87366280+YasinDeger48@users.noreply.github.com>
2024-09-15 23:16:01 +0300

    first react version added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './kubernetes.svg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,32 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+         DevOps Engineering Bootcamp - 2024
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docker.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Docker
+        </a>
+        <a
+          className="App-link"
+          href="https://kubernetes.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to Kubernetes
+        </a>
+        <a
+          className="App-link"
+          href="https://ansible.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to Ansible
         </a>
       </header>
     </div>
